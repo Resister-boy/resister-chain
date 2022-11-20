b8a566a48c32b61bde103442e0e5fe6815cd5603
Resister-boy <hassanpumped17@gmail.com>
2022-11-21 01:25:46 +0900

    create web server test

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	"./blockchain/blockchain.go"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello, It's Home!")
+}
+
 func main() {
-	chain := blockchain.getBlockchain()	// Blockchain Generate Func
-	chain.AddBlock("Second Block") // Generate Block
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Forth Block")
-	chain.AddBlock("Fifth Block")
-	for _, block := range chain.AllBlocks() { // Block Data 출력
-		fmt.Printf("Data : %s\n", block.Data)
-		fmt.Printf("Hash : %s\n", block.Hash)
-		fmt.Printf("Previous Hash : %s\n", block.Hash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
